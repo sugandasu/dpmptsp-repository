6ed97978f0e2881d03cf9b9b6c2dbe5c04fa079a
Arif Suganda <arifsuganda.su@gmail.com>
2021-11-27 16:13:38 +0800

    create field input

---
+++ frontend/src/components/FieldInput.tsx
@@ -0,0 +1,42 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { useField } from "formik";
+import { InputHTMLAttributes } from "react";
+import { IconType } from "react-icons";
+
+type FieldInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  LeftIcon?: IconType;
+};
+
+export const FieldInput: React.FC<FieldInputProps> = ({
+  label,
+  size: _,
+  LeftIcon,
+  ...props
+}) => {
+  const [field, { error, touched }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error} my={5}>
+      <FormLabel htmlFor={field.name} mb={2}>
+        {label}
+        {props.required ? <sup> *</sup> : null}
+      </FormLabel>
+      <InputGroup>
+        {LeftIcon ? (
+          <InputLeftElement color="gray.300" children={<LeftIcon />} />
+        ) : null}
+        <Input {...field} {...props} id={field.name} borderWidth={2} />
+      </InputGroup>
+      {touched && error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
