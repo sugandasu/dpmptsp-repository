3511695b93d1b273174dfe8bbb0796c10f551071
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-01 17:41:59 +0800

    create izin entitties

---
+++ backend/src/entities/Izin.ts
@@ -0,0 +1,37 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+export enum IzinTypes {
+  PERORANGAN = "Perorangan",
+  PERUSAHAAN = "Perusahaan",
+}
+
+@Entity()
+export class Izin extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", unique: true })
+  number!: string;
+
+  @Column({ type: "enum", enum: IzinTypes, default: IzinTypes.PERORANGAN })
+  type!: IzinTypes;
+
+  @Column()
+  name!: string;
+
+  @Column({ type: "date" })
+  effectiveDate!: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
