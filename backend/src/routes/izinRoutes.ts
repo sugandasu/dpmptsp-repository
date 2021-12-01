a09b47eae2a78d10c8d5221597d5be27d263ab84
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-01 17:42:26 +0800

    create izin routes

--- backend/src/routes/izinRoutes.ts
+++ backend/src/routes/izinRoutes.ts
@@ -1,7 +1,75 @@
 import express from "express";
+import { getConnection } from "typeorm";
+import { createIzinSchema, updateIzinSchema } from "../schemas/izinSchema";
+import { formatJoiError } from "../utils/formatJoiError";
+import { Izin } from "./../entities/Izin";
 
 export const izinRoutes = express.Router();
 
 izinRoutes.get("/", (_, res) => {
   res.send("Hello world! Izin!");
+});
+
+izinRoutes.post("/", async (req, res) => {
+  try {
+    const { number, type, name, effective_date } =
+      await createIzinSchema.validateAsync(req.body);
+    const izin = await Izin.create({
+      number,
+      type,
+      name,
+      effectiveDate: effective_date,
+    }).save();
+    return res.json({
+      izin: izin,
+      message: "Pembuatan izin berhasil",
+    });
+  } catch (err) {
+    return res.status(422).json({ errors: formatJoiError(err) });
+  }
+});
+
+izinRoutes.put("/:id", async (req, res) => {
+  try {
+    const { number, type, name, effective_date } =
+      await updateIzinSchema.validateAsync(req.body);
+    const izin = await Izin.findOne({ number: number });
+    if (izin && izin.id !== parseInt(req.params.id)) {
+      return res.status(422).json({
+        errors: { number: "Izin dengan nomor tersebut telah tersimpan" },
+      });
+    }
+
+    await getConnection()
+      .createQueryBuilder()
+      .update(Izin)
+      .set({ number, type, name, effectiveDate: effective_date })
+      .where("id = :id", {
+        id: req.params.id,
+      })
+      .execute();
+
+    return res.json({
+      izin: {
+        number: number,
+        type: type,
+        name: name,
+        effectiveDate: effective_date,
+      },
+      message: "Pembuatan izin berhasil",
+    });
+  } catch (err) {
+    return res.status(422).json({ errors: formatJoiError(err) });
+  }
+});
+
+izinRoutes.delete("/:id", async (req, res) => {
+  try {
+    await Izin.delete({ id: parseInt(req.params.id) });
+    return res.json({
+      message: "Penghapusan izin berhasil",
+    });
+  } catch (err) {
+    return res.status(422).json({ errors: { all: "Penghapusan izin gagal" } });
+  }
 });
---
+++ backend/src/schemas/izinSchema.ts
@@ -0,0 +1,65 @@
+import joi from "joi";
+import { Izin, IzinTypes } from "../entities/Izin";
+import { enumToArray } from "../utils/enumToArray";
+
+export const createIzinSchema = joi.object({
+  number: joi
+    .string()
+    .required()
+    .external(async (value) => {
+      const izin = await Izin.findOne({ where: { number: value } });
+      if (izin) {
+        throw new joi.ValidationError(
+          "string.exists",
+          [
+            {
+              message: "Izin dengan nomor tersebut telah tersimpan",
+              path: ["number"],
+              type: "string.exists",
+              context: {
+                key: "number",
+                label: "number",
+                value,
+              },
+            },
+          ],
+          value
+        );
+      }
+    })
+    .messages({
+      "any.required": "Nomor izin dibutuhkan",
+    }),
+  type: joi
+    .string()
+    .required()
+    .valid(...enumToArray(IzinTypes))
+    .messages({
+      "any.required": "Tipe izin dibutuhkan",
+      "any.valid": "Tipe izin tidak valid",
+    }),
+  name: joi.string().required().messages({ "any.required": "Nama dibutuhkan" }),
+  effective_date: joi.date().required().messages({
+    "any.required": "Tanggal belaku dibutuhkan",
+    date: "Format tanggal salah",
+  }),
+});
+
+export const updateIzinSchema = joi.object({
+  number: joi.string().required().messages({
+    "any.required": "Nomor izin dibutuhkan",
+  }),
+  type: joi
+    .string()
+    .required()
+    .valid(...enumToArray(IzinTypes))
+    .messages({
+      "any.required": "Tipe izin dibutuhkan",
+      "any.valid": "Tipe izin tidak valid",
+    }),
+  name: joi.string().required().messages({ "any.required": "Nama dibutuhkan" }),
+  effective_date: joi.date().required().messages({
+    "any.required": "Tanggal belaku dibutuhkan",
+    date: "Format tanggal salah",
+  }),
+});
