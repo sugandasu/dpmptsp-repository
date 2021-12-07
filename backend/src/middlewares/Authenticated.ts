c4577dddcbf0c12e123b9d2356ccb38e8375ba8e
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-07 14:41:35 +0800

    create authenticated middleware

---
+++ backend/src/middlewares/Authenticated.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+export const authenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorization = req.headers["authorization"];
+
+  if (!authorization) {
+    return res.status(401).json({
+      message: "Unauthenticated",
+    });
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const payload = verify(token, process.env.TOKEN_SECRET);
+    req.user = payload as any;
+  } catch (err) {
+    return res.status(401).json({
+      message: "Unauthenticated",
+    });
+  }
+
+  return next();
+};
