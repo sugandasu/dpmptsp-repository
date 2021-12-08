a6136bfb874ca4ad9d38346f548cd9c156896112
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-08 08:06:14 +0800

    redirect to next if exist

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Flex, Heading, VStack } from "@chakra-ui/react";
 import { AxiosResponse } from "axios";
 import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { AlertAll } from "../components/AlertAll";
@@ -10,6 +11,7 @@ import { request } from "../utils/request";
 
 const Login = () => {
   const [errorAll, setErrorAll] = useState("");
+  const router = useRouter();
   return (
     <Flex height="100vh" width="100%" align="center" justify="center">
       <VStack spacing={1}>
@@ -28,11 +30,16 @@ const Login = () => {
                 })
                 .then((response: AxiosResponse) => {
                   request.setAccessToken(response.data.accessToken);
+                  if (router.query.next) {
+                    router.push(router.query.next as string);
+                  } else {
+                    router.push("/dashboard");
+                  }
                 })
                 .catch((err) => {
                   setErrors(err.response.data.errors);
-                  if (err.response.data.errors?.all) {
-                    setErrorAll(err.response.data.errors.all);
+                  if (err.response.data.errors?.message) {
+                    setErrorAll(err.response.data.message);
                   }
                 })
                 .finally(() => {
