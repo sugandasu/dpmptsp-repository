985ee8ffab65b0d111c0e101ef6815b0f5282f28
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-08 08:06:34 +0800

    delete unused import

--- backend/src/routes/izinRoutes.ts
+++ backend/src/routes/izinRoutes.ts
@@ -1,9 +1,5 @@
 import express from "express";
 import izinController from "src/controllers/izinController";
-import { getConnection, getRepository } from "typeorm";
-import { createIzinSchema, updateIzinSchema } from "../schemas/izinSchema";
-import { formatJoiError } from "../utils/formatJoiError";
-import { Izin } from "./../entities/Izin";
 
 export const izinRoutes = express.Router();
 
