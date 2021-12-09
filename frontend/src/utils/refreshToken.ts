32f5424c4abebb06c9dc9575890cc6ffbae5ad9e
Arif Suganda <arifsuganda.su@gmail.com>
2021-12-09 18:53:55 +0800

    change link to refresh-token

--- frontend/src/utils/refreshToken.ts
+++ frontend/src/utils/refreshToken.ts
@@ -5,7 +5,7 @@ export const refreshToken = async () => {
   return await request
     .sendRequest({
       method: "POST",
-      url: process.env.NEXT_PUBLIC_API_URL + "/auth/refresh_token",
+      url: process.env.NEXT_PUBLIC_API_URL + "/auth/refresh-token",
       data: {},
     })
     .then((response: AxiosResponse) => {
